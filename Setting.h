8f7c965335eb07dac4f41c2b576c1283a28dad87
gazandic <gazandic@gmail.com>
2015-03-17 20:50:16 +0700

    Create Setting.h

---
+++ Setting.h
@@ -0,0 +1,12 @@
+class Setting{
+	public:
+		static void setAll();
+		static void setEkspresi();
+		static void setBilangan();
+		static void setOperator();
+		static int modeOperator;
+		static int modeEkspresi;
+		static int modeBilangan; 
+
+
+};
